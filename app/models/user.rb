3016c0d774ad22e65b8d04b16f746c270ade2247
Mario Alberto Chavez <mario.chavez@gmail.com>
2010-08-06 12:31:05 -0700

    Se implemento completamente paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   has_many :publications
   
-  has_attached_file :avatar 
-  #, :styles => { :medium => "300x300>", :thumb => "50x50>" }
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "50x50>" }
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
@@ -10,5 +9,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :avatar
 end
