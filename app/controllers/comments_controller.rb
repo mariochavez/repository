a4adcdc9845ee18edadf51b0976d52941122d909
Mario Alberto Chavez <mario.chavez@gmail.com>
2010-08-06 22:31:43 -0700

    Se implemento funcionalidad para eliminar comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,29 @@
 class CommentsController < ApplicationController
+  before_filter do
+    @publication = Publication.find params[:publication_id]
+  end
+  
   def create
-    publication = Publication.find params[:publication_id]
     comment = Comment.new params[:comment]
     comment.user = current_user
-    publication.comments << comment
-    publication.save
-     
-    redirect_to publication
+    @publication.comments << comment
+    
+    if @publication.save
+      redirect_to @publication, :notice => 'Comment was successfully created.'
+    else
+      redirect_to @publication, :notice => 'Comment body is missing, comment was not created.'
+    end
   end
+  
+  def destroy
+    comment = Comment.find params[:id]
+    
+    if current_user == @publication.user || current_user == comment.user
+      comment.destroy
+      redirect_to @publication, :notice => 'Comment was successfully deleted.'
+    else
+      redirect_to @publication, :notice => 'You are not allowed to deleted the comment.'
+    end
+  end
+    
 end
